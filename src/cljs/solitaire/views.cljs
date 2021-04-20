f01c36ed5046a52b9e2635400dca78212b9679dd
João Luis Moraes <jonmoraesnl@gmail.com>
2021-04-20 01:29:35 -0300

    exercise 1

--- src/cljs/solitaire/views.cljs
+++ src/cljs/solitaire/views.cljs
@@ -4,11 +4,12 @@
             [solitaire.events :as events]
             [solitaire.subs :as subs]))
 
-(defn hello [{:keys [who color]}]
+(defn hello [{:keys [who color bold?]}]
   [:p {:style {:color color}}
-   "Hello, " who "!"])
+   "Hello, " 
+   (if bold? [:b who] who) "!"])
 
 (defn main-panel []
   [:div
    [hello {:who "World", :color "green"}]
-   [hello {:who "re:Clojure", :color "red"}]])
+   [hello {:who "re:Clojure", :color "red", :bold? true}]])
