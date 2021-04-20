0d7516dd16096f569aba5a33a1f33fb6eb2bca26
João Luis Moraes <jonmoraesnl@gmail.com>
2021-04-20 11:45:07 -0300

    exercise 2

--- src/cljs/solitaire/views.cljs
+++ src/cljs/solitaire/views.cljs
@@ -1,15 +1,33 @@
 (ns solitaire.views
   (:require [clojure.string :as string]
             [re-frame.core :as rf]
+            [solitaire.board :as board]
             [solitaire.events :as events]
             [solitaire.subs :as subs]))
 
-(defn hello [{:keys [who color bold?]}]
-  [:p {:style {:color color}}
-   "Hello, " 
-   (if bold? [:b who] who) "!"])
+(defn field-blocked []
+  [:div.field.field--blocked])
+
+(defn field-empty []
+  [:div.field])
+
+(defn field-peg []
+  [:div.field
+   [:div.peg]])
+
+(defn field-view [type]
+  (case type
+    :blocked [field-blocked]
+    :empty [field-empty]
+    :peg [field-peg]))
+
+(defn board-view [board]
+  (let [[width height] (board/dimensions board)]
+    (into [:div.board]
+          (for [row board
+                field row]
+            [field-view field]))))
 
 (defn main-panel []
   [:div
-   [hello {:who "World", :color "green"}]
-   [hello {:who "re:Clojure", :color "red", :bold? true}]])
+   [board-view board/initial-board]])
